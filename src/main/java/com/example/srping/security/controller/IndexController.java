33ff63b846b1bac2345a985e109ef6e15b20a6b9
GyuYoung Park <gy971224@gmail.com>
2021-10-17 13:41:29 +0900

    시큐리티 설정

--- src/main/java/com/example/srping/security/controller/IndexController.java
+++ src/main/java/com/example/srping/security/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.srping.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -9,6 +10,37 @@ public class IndexController {
     @GetMapping({"", "/"})
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 해당주소를 낚아채버림 - SecurityConfig 파일 생성 후 작동안함.
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료";
     }
 
 }
