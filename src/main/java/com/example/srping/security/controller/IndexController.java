9cbbb0aa445ad35a98518cf435ce132a0e168b92
GyuYoung Park <gy971224@gmail.com>
2021-10-17 14:05:22 +0900

    시큐리티 회원가입

--- src/main/java/com/example/srping/security/controller/IndexController.java
+++ src/main/java/com/example/srping/security/controller/IndexController.java
@@ -1,11 +1,20 @@
 package com.example.srping.security.controller;
 
+import com.example.srping.security.domain.User;
+import com.example.srping.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping({"", "/"})
     public String index() {
@@ -13,34 +22,43 @@ public class IndexController {
     }
 
     @GetMapping("/user")
-    public @ResponseBody String user() {
+    public @ResponseBody
+    String user() {
         return "user";
     }
 
     @GetMapping("/admin")
-    public @ResponseBody String admin() {
+    public @ResponseBody
+    String admin() {
         return "admin";
     }
 
     @GetMapping("/manager")
-    public @ResponseBody String manager() {
+    public @ResponseBody
+    String manager() {
         return "manager";
     }
 
     // 스프링 시큐리티가 해당주소를 낚아채버림 - SecurityConfig 파일 생성 후 작동안함.
-    @GetMapping("/login")
-    public @ResponseBody String login() {
-        return "login";
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public @ResponseBody String join() {
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
     }
 
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc() {
-        return "회원가입 완료";
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = passwordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/loginForm";
     }
 
 }
---
+++ src/main/java/com/example/srping/security/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.srping.security.repository;
+
+import com.example.srping.security.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// CRUD 함수를 JpaRepository가 들고 있음.
+// @Repository라는 어노테이션이 없어도 IoC 된다. JpaRepository를 상속했기 때문에
+public interface UserRepository extends JpaRepository<User, Long> {
+}
